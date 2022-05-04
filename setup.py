006d5ab8a449128e50fa0c37aeb96bb1537f2238
VeaNae1 <100738731+VeaNae1@users.noreply.github.com>
2022-05-04 09:23:24 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup (
+  include_package_data = True,
+  name='mycalc0001',
+  verson='0.0.1',
+  description='oss-dev my calculator 0001',
+  author='VeaNae',
+  author_email='jess106@naver.com',
+  url="https://github.com/VeaNae1/mycalculator.git"
+  install_requires=['pytest'],
+  long_description='oss-dev calculator python module',
+  long_description_content_type='test/markdown',
+  classifiers=[
+      "Programming Language :: python :: 3"
+      "Operating System :: OS Independent",
+  ],
+)
